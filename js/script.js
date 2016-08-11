fd1dd23e4aef2cb339043c977f99119a8b3d71a9
Андрей Косых <a.kosyh@pflb.ru>
2016-08-11 10:11:55 +0300

    почистил от лишнего

--- js/script.js
+++ js/script.js
@@ -3,13 +3,9 @@ var config = {
         authorization: false
     },
     text_error_not_authorization: 'Пожалуйста, авторизуйтесь',
-    step: {},
     tabId: 0,
-    csrf_token: '',
-    _ym_uid: '',
     url: 'http://localhost:9797'
     //url: 'http://192.168.2.121:9797/'
-
 };
 
 function init() {
@@ -46,7 +42,6 @@ function init() {
             updateView();
         }
     });
-
 }
 
 function updateView() {
@@ -59,6 +54,7 @@ function updateView() {
     }
 }
 
+//TODO-front: переписать открытие окон
 function openPage(url) {
     if (url.split(':')[0] == 'http' || url.split(':')[0] == 'https') {
         chrome.tabs.create({url: url}, function (tabs) {
@@ -79,6 +75,7 @@ init();
 function setDom(code, tabId) {
     chrome.tabs.executeScript(tabId, {code: code});
 }
+
 //Работа с Background.js
 function setBackground(eventPage, object, objWin, url) {
     chrome.runtime.sendMessage({eventPage: eventPage, obj: object, objWin: objWin, url: url}, function (obj) {
@@ -87,6 +84,7 @@ function setBackground(eventPage, object, objWin, url) {
     });
 }
 
+//Постоянный порт на все страницы
 var runtimePort;
 chrome.runtime.onConnect.addListener(function (port) {
     runtimePort = port;
@@ -100,120 +98,3 @@ chrome.runtime.onConnect.addListener(function (port) {
         }
     });
 });
-
-/*
- function init() {
- $('.nav-tabs a').click(function (e) {
- e.preventDefault();
- $(".tab-pane").hide();
- $($(this).attr('href')).show();
- });
-
- $('.rec_btn').click(function () {
- setBackground("pageRecId", '0', '0');
- //TODO-front: проверить нет ли открытых
- chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
- $('.rec_btn').append(tabs.id);
- setBackground("pageRecId", tabs.id, tabs.windowId);
- });
- setBackground("startRec", 'init', '');
- function queryTab(tab) {
- console.log('открытый TAB', tab);
- chrome.tabs.query({}, function (array_of_Tabs) {
- var id_tab_rec = '';
- var id_tab_rec_window_id = '';
- for (var i in array_of_Tabs) {
- console.log(array_of_Tabs[i])
- if (array_of_Tabs[i].title == "https://сгвэо.рф/test/index.html") {
- id_tab_rec = array_of_Tabs[i].id;
- id_tab_rec_window_id = array_of_Tabs[i].index;
- console.log(id_tab_rec)
- }
- }
- $('.stop_btn').append(id_tab_rec);
- setBackground("idActiveTab", tab, '');
- });
- }
-
- chrome.tabs.create({active: false, url: 'https://xn--b1ab8aj6d.xn--p1ai/test/index.html'}, function (tab) {
- queryTab(tab)
- });
- });
-
- $('.stop_btn').click(function () {
- setBackground("stopRec");
- });
-
-
- $.ajax({
- type: "GET",
- url: "http://uxcrowd.ru:8081/api/tester/new-tasks",
- success: function (all_task) {
- console.log('1');
- var task_dom = "";
- for (var task in all_task) {
- if (window.location.href.indexOf(all_task[task].url) != -1) {
- chrome.tabs.getSelected(null, function (tab) { //выбирается ид открытого таба, выполняется коллбек с ним
- chrome.tabs.sendMessage(tab.id, {msg: all_task[task]}); //запрос  на сообщение
- });
- }
- task_dom += "<div class='col-xs-12'><div class='row'><div class='col-xs-2'>" + all_task[task].id + "</div><div class='col-xs-10'>" + all_task[task].url + "</div></div></div>";
- }
- $('#task').html(task_dom);
- },
- error: function (data) {
- console.log('2');
- $('#task').text(data);
- }
- });
- }
-
-
- // Работа с DOM (injected.js)
- function setDom(code) {
- chrome.tabs.getSelected(null, function (tab) {
- chrome.tabs.executeScript(tab.id, {code: code});
- });
- }
- //Работа с Background.js
- function setBackground(eventPage, object, objWin) {
- chrome.runtime.sendMessage({eventPage: eventPage, obj: object, objWin: objWin}, function (obj) {
- console.log('Ответ от фоновой странице:', obj.text);
- return obj;
- });
- }
-
- init();
-
-
- $('.btn_login').click(function () {
- $.ajax({
- type: "GET",
- url: "http://uxcrowd.ru:8081/api/authentication",
- method: "POST",
- data: {
- 'username': 'win7tester',
- 'password': '!QAZxsw2',
- 'remember-me': true,
- 'submit': 'Login'
- },
- success: function (all_task) {
- console.log('1');
- var task_dom = "";
- for (var task in all_task) {
- if (window.location.href.indexOf(all_task[task].url) != -1) {
- chrome.tabs.getSelected(null, function (tab) { //выбирается ид открытого таба, выполняется коллбек с ним
- chrome.tabs.sendMessage(tab.id, {msg: all_task[task]}); //запрос  на сообщение
- });
- }
- task_dom += "<div class='col-xs-12'><div class='row'><div class='col-xs-2'>" + all_task[task].id + "</div><div class='col-xs-10'>" + all_task[task].url + "</div></div></div>";
- }
- $('#task').html(task_dom);
- },
- error: function (data) {
- console.log('2');
- $('#task').text(data);
- }
- });
- });
- */
